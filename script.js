51c8c79a7dc2ec0f8d8904034579429df34e0b5d
william hill <williamhill0831@Hotmail.com>
2023-07-23 19:04:58 -0400

    Finished weather api

--- script.js
+++ script.js
@@ -1,37 +1,41 @@
-const searchCity = document.getElementById('search');
 const weatherContainer = document.querySelector('.weatherContainer');
 const description = document.querySelector('.description');
+const searchBtn = document.getElementById('searchBtn');
 
-const { log } = console;
-log(searchCity);
-// key 96341608d1a046c7b04181727232007
-searchCity.addEventListener('input', (e) => {
-  const searchValue = e.target.value;
-  log(searchValue);
+searchBtn.addEventListener('click', (e) => {
+  e.preventDefault();
+  const searchCity = document.querySelector('#search').value;
+  fetchData(searchCity);
 });
 
+function fetchData(search) {
+  const apiUrl = `https://api.weatherapi.com/v1/current.json?key=43433c1a5764250bfd30625232307&q=${encodeURIComponent(
+    search,
+  )}`;
 
-  .then((response) => response.json())
-  .then((data) => {
-    const card = weatherContainer;
-    const weatherCondition = card.querySelector('.condition');
-    const city = card.querySelector('.city');
-    const temp = card.querySelector('.temp');
-    const weatherDetails = description;
-    const detail1 = weatherDetails.querySelector('.one');
-    const detail2 = weatherDetails.querySelector('.two');
-    const detail3 = weatherDetails.querySelector('.three');
+  fetch(apiUrl)
+    .then((response) => {
+      if (!response.ok) throw new Error('not valid response');
+      return response.json();
+    })
+    .then((data) => {
+      const card = weatherContainer;
+      const weatherCondition = card.querySelector('.condition');
+      const city = card.querySelector('.city');
+      const temp = card.querySelector('.temp');
+      const weatherDetails = description;
+      const detail1 = weatherDetails.querySelector('.one');
+      const detail2 = weatherDetails.querySelector('.two');
+      const detail3 = weatherDetails.querySelector('.three');
 
-    weatherCondition.textContent = data.current.condition.text;
-    city.textContent = `${data.location.name}, ${data.location.country}`;
-    temp.innerHTML = `${data.current.temp_c} <span>&#8451;<span>`;
-    detail1.innerHTML = `FEELS LIKE: ${data.current.feelslike_c}<span>&#8451;<span>`;
-    detail2.textContent = `WIND: ${data.current.wind_kph} km/h`;
-    detail3.textContent = `HUMIDITY: ${data.current.humidity}%`;
-
-    log(card);
-    log(data);
-    log(weatherCondition);
-  });
+      weatherCondition.textContent = data.current.condition.text;
+      city.textContent = `${data.location.name}, ${data.location.country}`;
+      temp.innerHTML = `${data.current.temp_c} <span>&#8451;</span>`;
+      detail1.innerHTML = `FEELS LIKE: ${data.current.feelslike_c}<span class='celsius'>&#8451;</span>`;
+      detail2.textContent = `WIND: ${data.current.wind_kph} km/h`;
+      detail3.textContent = `HUMIDITY: ${data.current.humidity}%`;
+    })
+    .catch((error) => console.log(error, '404 status'));
+}
 
 // .then((data) => console.log(data.current.temp_c));
